0b5912933ddb2b6351019a8be2d8d0f4a6e84783
Anabelle <bellech88@gmail.com>
2021-03-09 00:56:54 -0500

    Updates

---
+++ precache-manifest.8be39a14c4a0eceb7e4aa4904a2b857b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c520f0c052fdd50fa4d5c063a358ac77",
+    "url": "/crime-navi/index.html"
+  },
+  {
+    "revision": "3188208382062bb39d37",
+    "url": "/crime-navi/static/css/main.0749616c.chunk.css"
+  },
+  {
+    "revision": "231080596f3fa2b4be28",
+    "url": "/crime-navi/static/js/2.942f4147.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crime-navi/static/js/2.942f4147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3188208382062bb39d37",
+    "url": "/crime-navi/static/js/main.ffe5e2d8.chunk.js"
+  },
+  {
+    "revision": "9980dbe6daeb149718d4",
+    "url": "/crime-navi/static/js/runtime-main.f7260810.js"
+  }
+]);
